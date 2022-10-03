ea932f2fb04b625401b951724c93adbc65375ab4
S P A R K Y <114299352+spa4ky@users.noreply.github.com>
2022-10-03 08:48:49 +0530

    Update language.py

--- jocasta/services/language.py
+++ jocasta/services/language.py
@@ -21,9 +21,7 @@ for filename in os.listdir("jocasta/languages"):
     with open("jocasta/languages/" + filename, "r", encoding="utf8") as f:
         lang = yaml.load(f, Loader=yaml.CLoader)
         lang_code = lang["language_info"]["code"]
-        lang_code.sort()
-        lang["language_info"]["babel"] = Locale(lang_code)
-        LANGUAGES[lang_code] = lang
+        LANGUAGES.sort(lang)
 
 log.debug(
     "Languages loaded: {}".format(
